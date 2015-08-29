3f5eb61748b27b930b663fc6ccc8f5942a3e8283
Miguel Guadarrama <miguelguadarrama@gmail.com>
2015-08-28 21:36:15 -0500

    CodeIgniter Setup & Bootstrap Files

---
+++ application/views/index.php
@@ -0,0 +1,27 @@
+<!doctype html>
+<html>
+<head>
+<base href="<?= base_url();?>">
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+<title>IBR</title>
+<link rel="stylesheet" href="templates/ibr2015/css/ibr.css">
+<link rel="stylesheet" href="templates/ibr2015/includes/bootstrap/css/bootstrap.min.css">
+<link rel="stylesheet" href="templates/ibr2015/includes/bootstrap/css/bootstrap-theme.min.css">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+<script src="templates/ibr2015/includes/bootstrap/js/bootstrap.min.js"></script>
+</head>
+<body>
+<div class="container">
+	<div class="header">
+        <div class="row">
+            <div class="col-xs-12">
+                <h1>It's Working!</h1>
+                <p class="alert alert-success"><span class="glyphicon glyphicon-ok"></span> CodeIgniter and Bootstrap has been succesfully installed!</p>
+                <p class="text-right"><span class="label label-default">CodeIgniter</span> <span class="label label-primary">Bootstrap</span> <span class="label label-danger">HTML5</span></p>
+            </div>
+        </div>
+    </div>
+</div>
+</body>
+</html>
