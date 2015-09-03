e466954088fce38f6e52ae9578a52b899e48608b
Miguel Guadarrama <miguelguadarrama@gmail.com>
2015-09-03 17:58:56 -0500

    test db

--- testdb.php
+++ testdb.php
@@ -1,6 +1,6 @@
 <?php
-ini_set('display_errors', 1);
-$link = mysqli_connect('', '', '', '');
+//ini_set('display_errors', 1);
+$link = mysqli_connect('us-cdbr-azure-east-b.cloudapp.net', 'b178002c03d52d', '6f9d6e3a', 'ibrdev');
 
 if(!$link){
 	echo mysqli_connect_error();
