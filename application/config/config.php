5bedf13922b1ae917b824daa6d05aa60887515d3
Miguel Guadarrama <miguelguadarrama@gmail.com>
2015-09-03 17:03:40 -0500

    URL azurewebsites

--- application/config/config.php
+++ application/config/config.php
@@ -17,7 +17,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | environments.
 |
 */
-$config['base_url'] = 'http://ibrdev.azurewebsites.net/';
+$config['base_url'] = 'https://ibrdev.azurewebsites.net/';
 
 /*
 |--------------------------------------------------------------------------
