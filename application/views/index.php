429cbc57608075d05368784b71827d304b5b8284
Wolfgang1984 <wolfgangalbeniz.romero@gmail.com>
2015-09-15 12:51:56 -0500

    Code Cleaning

--- application/views/index.php
+++ application/views/index.php
@@ -10,17 +10,7 @@
 <link rel="stylesheet" href="<?= base_url("templates/ibr2015/includes/bootstrap/css/bootstrap-theme.min.css");?>">
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
 <script src="<?= base_url("templates/ibr2015/includes/bootstrap/js/bootstrap.min.js")?>"></script>
-<!--<script>
-	$(document).ready(function(){
-		$("#gohome").on('click', function(){
-			window.location = "/";
-		});
-	$('#info').click(function (e) {
-  e.preventDefault()
-  $('#message').html('<div class="alert alert-info fade in"><button type="button" class="close close-alert" data-dismiss="alert" aria-hidden="true">×</button>Array!!</div>');
-})
-	});
-</script>-->
+
 <style>
 body {
   min-height: 2000px;
@@ -45,25 +35,7 @@ body {
           <ul class="nav navbar-nav">
             <li class="active"><a href="http://ibrdev.azurewebsites.net/">Home</a></li>
             <li><a href="https://docs.google.com/forms/d/1ZSNuMGHvsBiWJpggjfXaCl9D7eAUqnj7GuvJB5aQ35M/viewform">Punch In</a></li>
-            <!--<li><a href="#contact">Contact</a></li>
-            <li class="dropdown">
-              <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <b class="caret"></b></a>
-              <ul class="dropdown-menu">
-                <li><a href="#">Action</a></li>
-                <li><a href="#">Another action</a></li>
-                <li><a href="#">Something else here</a></li>
-                <li class="divider"></li>
-                <li class="dropdown-header">Nav header</li>
-                <li><a href="#">Separated link</a></li>
-                <li><a href="#">One more separated link</a></li>
-              </ul>
-            </li>-->
           </ul>
-          <!--<ul class="nav navbar-nav navbar-right">
-            <li><a href="http://www.joyofbootstrap.com/output/navbardefault.php">Default</a></li>
-            <li><a href="http://www.joyofbootstrap.com/output/navbarstatic.php">Static top</a></li>
-            <li class="active"><a href="http://www.joyofbootstrap.com/output/navbarfixed.php">Fixed top</a></li>
-          </ul>-->
         </div><!--/.nav-collapse -->
       </div>
     </div>
@@ -72,7 +44,7 @@ body {
 
       <!-- Main component for a primary marketing message or call to action -->
       <div class="jumbotron">
-        <h1>Aplicaión en desarrollo</h1>
+        <h1>Aplicación en Desarrollo</h1>
         <p>Este proyecto está creciendo ante sus ojos, pronto muchas más características serán añadidas. Mientras tanto sírvase a entrar en la opción “Punch in” del menú de navegación para reportar su participación en el proyecto.</p>
 
       </div>
@@ -83,17 +55,13 @@ body {
 	<div class="header">
         <div class="row">
             <div class="col-xs-12">
-                <!--<h1>Mike and Chichito's Love Affair!</h1>-->
-               <!-- <p class="alert alert-success"><span class="glyphicon glyphicon-ok"></span> CodeIgniter and Bootstrap has been succesfully installed!</p>-->
                
             </div>
         </div>
         <hr>
-        <!--<a href="/" class="btn btn-lg btn-primary">asdasd</a>
-        <button type="button" class="btn btn-lg btn-info" id="info">["hip", "hip"]</button>
-        <button id="gohome" class="btn btn-danger">Otro</button>-->
+
     </div>
-    <!--<div id="message"></div>-->
+
 </div>
 </body>
 <footer>
