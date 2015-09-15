07ebe75071d9b2852c3d0084e7af2a1b17873845
Wolfgang1984 <wolfgangalbeniz.romero@gmail.com>
2015-09-15 12:33:05 -0500

    Modifications to header

--- application/views/index.php
+++ application/views/index.php
@@ -39,13 +39,13 @@ body {
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
           </button>
-          <a class="navbar-brand" href="#">Project name</a>
+          <a class="navbar-brand" href="#">IBR R2M</a>
         </div>
         <div class="navbar-collapse collapse">
           <ul class="nav navbar-nav">
             <li class="active"><a href="#">Home</a></li>
-            <li><a href="#about">About</a></li>
-            <li><a href="#contact">Contact</a></li>
+            <li><a href="#about">Punch In</a></li>
+            <!--<li><a href="#contact">Contact</a></li>
             <li class="dropdown">
               <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <b class="caret"></b></a>
               <ul class="dropdown-menu">
@@ -57,13 +57,13 @@ body {
                 <li><a href="#">Separated link</a></li>
                 <li><a href="#">One more separated link</a></li>
               </ul>
-            </li>
+            </li>-->
           </ul>
-          <ul class="nav navbar-nav navbar-right">
+          <!--<ul class="nav navbar-nav navbar-right">
             <li><a href="http://www.joyofbootstrap.com/output/navbardefault.php">Default</a></li>
             <li><a href="http://www.joyofbootstrap.com/output/navbarstatic.php">Static top</a></li>
             <li class="active"><a href="http://www.joyofbootstrap.com/output/navbarfixed.php">Fixed top</a></li>
-          </ul>
+          </ul>-->
         </div><!--/.nav-collapse -->
       </div>
     </div>
