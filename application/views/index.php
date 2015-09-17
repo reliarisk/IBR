63c3726004f71f5cfe217941de612560bf47614c
Wolfgang1984 <wolfgangalbeniz.romero@gmail.com>
2015-09-17 13:00:28 -0500

    Loosing my mind with this minor fixes

--- application/views/index.php
+++ application/views/index.php
@@ -68,10 +68,12 @@ body {
         
                <div id="container-2">
 
-               <video width="600" controls> 
-					<source src="templates/ibr2015/video/Demo.mp4" type="video/mp4">
-			 </video>
              
+                     <video width="600" controls>
+                      <source src="\templates\ibr2015\video\Demo.mp4" type="video/mp4">
+                      Your browser does not support HTML5 video.
+                    </video>
+                     
             </div>
 
         </div> <!-- /container -->
